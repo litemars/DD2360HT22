6bc579768de3b700234ca55ae0ec6b4dc3c766b2
Massimo Bertocchi <masber@kth.se>
2022-11-29 20:00:36 +0100

    almost finish part 1

--- assignment3/lab3_ex1_template.cu
+++ assignment3/lab3_ex1_template.cu
@@ -2,16 +2,17 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <stdlib.h>
-#include <curand_kernel.h>
-#include <curand.h>
 
 #define DataType double
 
+
 __global__ void vecAdd(DataType *in1, DataType *in2, DataType *out, int len) {
   //@@ Insert code to implement vector addition here
-  // need to check why
     int id = blockDim.x * blockIdx.x + threadIdx.x;
-    if(id < len) out[id] = in1[id] + in2[id];
+    
+    if(id < len) {
+      out[id] = in1[id] + in2[id];
+      }
 }
 
 //@@ Insert code to implement timer start
@@ -27,10 +28,16 @@ double stopTimer(double startime) {
    return (((double)tp.tv_sec + (double)tp.tv_usec*1.e-6) - startime);
 }
 
+double RandomReal(double low, double high)
+{
+  double d;
 
+  d = (double) rand() / ((double) RAND_MAX + 1);
+  return (low + d * (high - low));
+}
 
 void printArray(double *in, int len){
-  for(int i=0;i<=len;i++){
+  for(int i=0;i<len;i++){
     printf("%f ",in[i]);
   }
   printf("\n\n");
@@ -61,40 +68,56 @@ int main(int argc, char **argv) {
   hostOutput = (DataType*)malloc(inputLength*sizeof(DataType));
   
   //@@ Insert code below to initialize hostInput1 and hostInput2 to random numbers, and create reference result in CPU
-  resultRef = (DataType*)malloc(inputLength*sizeof(DataType));
 
+  for(int i=0;i<inputLength;i++){
+      hostInput1[i]=RandomReal(0,1);
+      hostInput2[i]=RandomReal(0,1);
+  }
+
+  resultRef = (DataType*)malloc(inputLength*sizeof(DataType));
+  double start_time=startTimer();
   for(int i=0;i<inputLength;i++){
     resultRef[i]=hostInput1[i]+hostInput2[i];
   }
-
+  double stopCPU=stopTimer(start_time);
   //@@ Insert code below to allocate GPU memory here
   cudaMalloc(&deviceInput1, inputLength*sizeof(DataType));
   cudaMalloc(&deviceInput2, inputLength*sizeof(DataType));
   cudaMalloc(&deviceOutput, inputLength*sizeof(DataType));
 
   //@@ Insert code to below to Copy memory to the GPU here
+  double start_mem=startTimer();
   cudaMemcpy(deviceInput1, hostInput1, inputLength*sizeof(DataType), cudaMemcpyHostToDevice);
   cudaMemcpy(deviceInput2, hostInput2, inputLength*sizeof(DataType), cudaMemcpyHostToDevice);
-
+  cudaDeviceSynchronize();
+  double stop_mem=stopTimer(start_mem);
   //@@ Initialize the 1D grid and block dimensions here
-  dim3 Dg(7,1,1)
-  dim3 Db(256,1,1)
+  dim3 Dg(1024,1,1);
+  dim3 Db(1024,1,1);
 
   //@@ Launch the GPU Kernel here
+  double start_time_gpu=startTimer();
   vecAdd<<<Dg,Db>>>(deviceInput1,deviceInput2,deviceOutput,inputLength);
+  cudaDeviceSynchronize();
+  double stopGPU=stopTimer(start_time_gpu);
 
-
+  double start_mem_2=startTimer();
   //@@ Copy the GPU memory back to the CPU here
   cudaMemcpy(hostOutput, deviceOutput, inputLength*sizeof(DataType), cudaMemcpyDeviceToHost);
+  cudaDeviceSynchronize();
+  double stop_mem_2=stopTimer(start_mem_2);
 
   //@@ Insert code below to compare the output with the reference
-  double tolerance = 1.0e-14;
   for(int i=0;i<inputLength;i++){
-    if(resultRef[i] - tolerance <= hostOutput[i] && resultRef[i] + tolerance >= hostOutput[i]){
-        printf("Error counting numbers");
+    if(resultRef[i] != hostOutput[i] && abs(resultRef[i]-hostOutput[i])>0.001 ){
+        printf("Error counting numbers: %f",abs(resultRef[i]-hostOutput[i]) );
         return 0;
     }
   }
+  printf("sum verified: Correct!\n");
+  printf("Time Host->Device: %f - Time Device->Host: %f\n",stop_mem,stop_mem_2);
+  printf("CPU time: %f - GPU time: %f\n",stopCPU,stopGPU);
+  
   //@@ Free the GPU memory here
   cudaFree(deviceInput1);
   cudaFree(deviceInput2);
