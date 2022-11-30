27f46227588be622b62475d596eabc69123f9b7f
Massimo Bertocchi <masber@kth.se>
2022-11-30 16:56:36 +0100

    ex2-part1

--- assignment3/lab3_ex2.cu
+++ assignment3/lab3_ex2.cu
@@ -8,19 +8,40 @@
 __global__ void gemm(DataType *A, DataType *B, DataType *C, int numARows,
                       int numAColumns, int numBRows, int numBColumns){
   //@@ Insert code to implement matrix multiplication here
-    int row = blockIdx.y * blockDim.y + threadIdx.y; 
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
-    int sum = 0;
     
-    if( col < k && row < m) 
+    int id = blockDim.x * blockIdx.x + threadIdx.x;
+    
+    if( id < numBColumns*numARows) 
     {
-        for(int i = 0; i < n; i++) 
-        {
-            sum += a[row * n + i] * b[i * k + col];
-        }
-        c[row * k + col] = sum;
-    }
+      printf("id %d\n",id);
+        C[id]=0;
+        for(int k=0;k<numARows;k++){
 
+            printf("id: %d : %d - %d\n",id,k*+id%numARows,id%numBColumns+k*numBColumns);
+            __syncthreads();
+            //C[id]+=A[k+col+row*numARows]*B[numBColumns*k];
+            }
+          }
+}
+
+void printArray(double *in, int row, int col){
+  printf("\ndims: %d-%d \n",row,col);
+  for(int i=0;i<col*row;i=i+col){
+    for(int j=0;j<col;j++){
+      printf("%f ",in[i+j]);
+    }
+    printf("\n");
+  }
+  printf("\n\n");
+
+}
+
+double RandomReal(double low, double high)
+{
+  double d;
+
+  d = (double) rand() / ((double) RAND_MAX + 1);
+  return (low + d * (high - low));
 }
 
 int main(int argc, char **argv) {
@@ -40,27 +61,76 @@ int main(int argc, char **argv) {
   int numCColumns;
 
   //@@ Insert code below to read in numARows, numAColumns, numBColumns from args
+   if (argc<4){
+    printf("input length invalid\n");
+    printf("./a.out numARows numAColumns numBColumns\n");
+    return 0;
+  }
+  
+  numARows=atoi(argv[1]);
+  numAColumns=atoi(argv[2]);
+  numBRows=atoi(argv[2]);
+  numBColumns=atoi(argv[3]);
+  numCRows=atoi(argv[1]);
+  numCColumns=atoi(argv[3]);
 
   printf("Input matrix dim (%d x %d) (%d x %d) (%d x %d)\n", numARows, numAColumns, numBRows, numBColumns, numCRows, numCColumns);
   
   //@@ Insert code below to allocate Host memory for input and output
-
+  hostA = (DataType*)malloc(numARows * numAColumns * sizeof(DataType));
+  hostB = (DataType*)malloc(numBRows * numBColumns * sizeof(DataType));
+  hostC = (DataType*)malloc(numCRows * numCColumns * sizeof(DataType));
   
   //@@ Insert code below to initialize hostA and hostB to random numbers, and create reference result in CPU
+  for(int i=0;i<numARows * numAColumns;i++){
+      hostA[i]=RandomReal(0,1);
+  }
+  for(int i=0;i<numBRows * numBColumns;i++){
+      hostB[i]=RandomReal(0,1);
+  }
+  printf("Matrix A:\n");
+  printArray(hostA,numARows,numAColumns);
 
+  printf("Matrix B:\n");
+  printArray(hostB,numBRows,numBColumns);
+
+  resultRef = (DataType*)malloc(numCRows * numCColumns * sizeof(DataType));
+
+  //A0*B(0+0row)+A1*B(0+1row)+A2*B(0+2row)=C0
+  //A0*B(1+0row)+A1*B(1+1row)+A2*B(1+2row)=C1
+  //
+  //A
+  for(int i=0; i<numARows;i++){
+    
+    for(int j=0;j<numBColumns;j++){
+      //printf("i: %d and j: %d\n",i,j);
+      for(int k=0;k<numAColumns;k++){
+        //printf("%d - %d\n",k+numBColumns*i,j+numBColumns*k);
+        resultRef[j+numBColumns*i]+=hostA[k+numBColumns*i]*hostB[j+numBColumns*k];
+      }
+      
+    }
+  }
+  printf("Matrix C:\n");
+  printArray(resultRef,numCRows,numCColumns);
 
   //@@ Insert code below to allocate GPU memory here
-
+  cudaMalloc(&deviceA, numARows * numAColumns * sizeof(DataType));
+  cudaMalloc(&deviceB, numBRows * numBColumns * sizeof(DataType));
+  cudaMalloc(&deviceC, numCRows * numCColumns * sizeof(DataType));
 
   //@@ Insert code to below to Copy memory to the GPU here
+  cudaMemcpy(deviceA, hostA, numARows * numAColumns*sizeof(DataType), cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceB, hostB, numBRows * numBColumns*sizeof(DataType), cudaMemcpyHostToDevice);
 
 
   //@@ Initialize the grid and block dimensions here
-
+  dim3 Dg(1024,1,1);
+  dim3 Db(1024,1,1);
 
   //@@ Launch the GPU Kernel here
-
-
+  gemm<<<Dg,Db>>>(deviceA,deviceB,deviceC,numARows,numAColumns,numBRows,numBColumns);
+  cudaDeviceSynchronize();
   //@@ Copy the GPU memory back to the CPU here
 
 
@@ -71,7 +141,9 @@ int main(int argc, char **argv) {
 
 
   //@@ Free the CPU memory here
-
+  free(hostA);
+  free(hostB);
+  free(hostC);
 
   return 0;
 }
